34a82b5b755b255342c1f5ea1d5ffb62832aa25e
zhassulan <zhzhussupovkz@gmail.com>
2015-07-08 17:32:52 +0600

    driving: add sound for main player

--- core/taxi.py
+++ core/taxi.py
@@ -8,6 +8,8 @@ from car import *
 class Taxi(Car):
     def __init__(self, pygame, screen, x, y):
         self.pygame = pygame
+        self.acc = pygame.mixer.Sound("./sounds/acc.ogg")
+        self.acc.set_volume(0.01)
         image = self.pygame.image.load("./images/cars/taxi.png")
         super(Taxi, self).__init__(screen, x, y, image)
 
@@ -18,8 +20,10 @@ class Taxi(Car):
         key = self.pygame.key.get_pressed()
         if key[self.pygame.K_UP]:
             self.go()
+            self.acc.play()
         elif key[self.pygame.K_DOWN]:
             self.brake()
+            self.acc.stop()
         if key[self.pygame.K_RIGHT]:
             self.move_right()
         elif key[self.pygame.K_LEFT]:
